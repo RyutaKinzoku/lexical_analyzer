3ea6fdd5af9beef7bf778d2d35c72bb33fb08d79
Kenneth Ibarra Vargas <66222501+RyutaKinzoku@users.noreply.github.com>
2022-04-04 17:16:47 -0600

    incomplete define

--- lexical_analyzer.c
+++ lexical_analyzer.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAXSTLEN 64
+#define MAXSTLEN 128
 typedef char string [MAXSTLEN];
 char token_buffer[MAXSTLEN];
 int token_buffer_index = 0;
 FILE *cCode;
 FILE *cTemp;
+
 
 void clear_buffer(void){
     memset(token_buffer, 0, sizeof token_buffer);
@@ -18,9 +19,15 @@ void clear_buffer(void){
 void buffer_char(char c){
     token_buffer[token_buffer_index] = c;
     token_buffer_index++;
+    if (token_buffer_index > MAXSTLEN-1)
+    {
+        printf("Buffer overflow\n");
+        exit(-1);
+    }
 }
 
 void preprocessing(string fileName){
+    printf("file %s\n", fileName);
     if (!strcmp(fileName, ""))
         return;
 
@@ -36,7 +43,6 @@ void preprocessing(string fileName){
     if(feof(file))
         return;
     while ((in_char = getc(file)) != EOF){
-        printf("hola %s \n", fileName);
         if (in_char == '#'){
             for (c = getc(file); isalpha(c); c = getc(file)){
                 buffer_char(c);
@@ -52,40 +58,98 @@ void preprocessing(string fileName){
                         quotes++;
                     }else if (c == '\n')
                     {
-                        printf("There are not enough quotes in the #include directive");
+                        printf("There are not enough quotes in the #include directive\n");
                         fclose(file);
                         exit(-1);
-                    } else if (c == '/' && c == 0)
+                    } else if (c == '/' || c == 0)
                     {
-                        printf("Invalid character in a file");
+                        printf("Invalid character in a file\n");
                         fclose(file);
                         exit(-1);
                     } else if (quotes >= 1)
                     {
                         buffer_char(c);
-                    }
-                    
-                }
-                while (c != '\n'){
-                    if (!isspace(c))
+                    } else if (!isspace(c))
                     {
-                        printf("Extra characters after #include directive");
+                        printf("Expected filename\n");
                         fclose(file);
                         exit(-1);
+                    }
+                }
+                int extraChar = 0;
+                while (c != '\n'){
+                    if (!isspace(c) && extraChar == 0)
+                    {
+                        printf("Warning: Extra characters after #include directive\n");
+                        extraChar++;
                     }
                     c = getc(file);
                 }
                 if (token_buffer_index == 0)
                 {
-                    printf("Expected filename");
+                    printf("Expected filename\n");
                     fclose(file);
                     exit(-1);
                 }
-
-                
-            } else if (!strcmp(token_buffer, "define"))
+                preprocessing(token_buffer);
+            } else if (!strcmp(token_buffer, "define"))//////////////////////////
             {
-                /* code */
+                clear_buffer();
+                int flag = 0;
+                for (c = getc(file); c != '\n'; c = getc(file)){
+                    if(flag == 0 && (isalpha(c) || c == '_')){
+                        buffer_char(c);
+                    }
+                    if (isspace(c) && flag == 0 && token_buffer_index > 0)
+                    {
+                        flag++;
+                    }
+                    
+                    if (isdigit(c) && token_buffer_index == 0)
+                    {
+                        printf("Macro names must be identifiers\n");
+                        fclose(file);
+                        exit(-1);
+                    }
+                    if (c == '\"')
+                    {   
+                        //quotes++;
+                    }else if (c == '\n')
+                    {
+                        printf("There are not enough quotes in the #include directive\n");
+                        fclose(file);
+                        exit(-1);
+                    } else if (c == '/' || c == 0)
+                    {
+                        printf("Invalid character in a file\n");
+                        fclose(file);
+                        exit(-1);
+                    } /*else if (quotes >= 1)
+                    {
+                        buffer_char(c);
+                    }*/ else if (!isspace(c))
+                    {
+                        printf("Expected filename\n");
+                        fclose(file);
+                        exit(-1);
+                    }
+                }
+                int extraChar = 0;
+                while (c != '\n'){
+                    if (!isspace(c) && extraChar == 0)
+                    {
+                        printf("Extra characters after #include directive\n");
+                        extraChar++;
+                    }
+                    c = getc(file);
+                }
+                if (token_buffer_index == 0)
+                {
+                    printf("Expected filename\n");
+                    fclose(file);
+                    exit(-1);
+                }
+                preprocessing(token_buffer);
             }else
             {
                 /* code */
