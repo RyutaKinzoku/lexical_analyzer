eaae4594c5188136d3b244a8e8f653819fb521f6
Reyner-Arias <Rey86581@gmail.com>
2022-04-22 17:47:17 -0600

    Beamer presentation initial slides

--- cTemp.c
+++ cTemp.c
@@ -1,10 +1,11 @@
-int hola;
-//test a a a
+int test(){
+printf("%d", 1);
+}
+int cinco = 5;
 int main(){
-int ah = q; #define q 1
-int w = b;
+int t = 10030;
 int r = hola();
 char f[10] = " a \n";
-int v = 13214;
+int v = 1;
 printf("%s", f);
-}
+} 
